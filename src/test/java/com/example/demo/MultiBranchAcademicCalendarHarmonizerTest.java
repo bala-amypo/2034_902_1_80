9ca18a81d11832d112704e081bda38c61cb671a6
bala-amypo <bala@amypo.in>
2025-12-26 09:31:35 +0000

    User commit

--- src/test/java/com/example/demo/MultiBranchAcademicCalendarHarmonizerTest.java
+++ src/test/java/com/example/demo/MultiBranchAcademicCalendarHarmonizerTest.java
@@ -599,7 +599,7 @@ public class MultiBranchAcademicCalendarHarmonizerTest {
         UserAccount ua = new UserAccount(15L, "E", "emailclaim@example.com",
                 "ENC_pwd", "REVIEWER", "Dept", LocalDateTime.now());
         String token = jwtUtil.generateTokenForUser(ua);
-        Object email = jwtUtil.parseToken(token).getPayload().get("email");
+        String email = jwtUtil.extractUsername(token);
         Assert.assertEquals(email, "emailclaim@example.com");
     }
 
@@ -608,7 +608,7 @@ public class MultiBranchAcademicCalendarHarmonizerTest {
         UserAccount ua = new UserAccount(16L, "R", "roleclaim@example.com",
                 "ENC_pwd", "ADMIN", "Dept", LocalDateTime.now());
         String token = jwtUtil.generateTokenForUser(ua);
-        Object role = jwtUtil.parseToken(token).getPayload().get("role");
+        String role = jwtUtil.extractRole(token);
         Assert.assertEquals(role, "ADMIN");
     }
 
@@ -617,7 +617,7 @@ public class MultiBranchAcademicCalendarHarmonizerTest {
         UserAccount ua = new UserAccount(17L, "I", "idclaim@example.com",
                 "ENC_pwd", "REVIEWER", "Dept", LocalDateTime.now());
         String token = jwtUtil.generateTokenForUser(ua);
-        Object id = jwtUtil.parseToken(token).getPayload().get("userId");
+        Long id = jwtUtil.extractUserId(token);
         Assert.assertNotNull(id);
     }
 
